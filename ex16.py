43cb4014c2eea97acd3e4e926380c43bf3af6892
Mason Hartman <zewlie@gmail.com>
2015-12-18 21:19:36 -0800

    Day 16 completed.

--- ex16.py
+++ ex16.py
@@ -49,5 +49,36 @@ def find_sue_num(sue_file, tape_file):
     return sues
 
 
-print find_sue_num('ex16.txt', 'ex16b.txt')
+def find_sue_num_2(sue_file, tape_file):
+    """Finds the Aunt Sue who meets the requirements specified by the MFCSAM output,
+    assuming the following:
+
+        * There must be a greater number of 'cats' and 'trees' than specified
+        * There must be a lesser number of pomeranians and goldfish than specified
+    """
+
+    sues = file_to_sue_dict(sue_file)
+    reqs = read_ticker_tape(tape_file)
+    keys = reqs.keys()
+
+    count = 0
+    while len(sues) > 1:
+        new_sues = {}
+        for sue in sues:
+            if keys[count] in ('cats', 'trees'):
+                if keys[count] not in sues[sue] or reqs[keys[count]] < sues[sue].get(keys[count]):
+                    new_sues[sue] = sues[sue]
+            elif keys[count] in ('pomeranians', 'goldfish'):
+                if keys[count] not in sues[sue] or reqs[keys[count]] > sues[sue].get(keys[count]):
+                    new_sues[sue] = sues[sue]
+            else:
+                if keys[count] not in sues[sue] or reqs[keys[count]] == sues[sue].get(keys[count]):
+                    new_sues[sue] = sues[sue]
+        sues = new_sues
+        count += 1
+
+    return sues
+
+
+print find_sue_num_2('ex16.txt', 'ex16b.txt')
 
